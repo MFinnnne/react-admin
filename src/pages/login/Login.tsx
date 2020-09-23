8188d4e4c3f19ee5816a8146c6af6763afb81bb1
MFine <1737374338@qq.com>
2020-09-24 01:04:25 +0800

    项目初始化

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+/**
+ *  登录的路由组件
+ *  Author: MFine
+ */
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        Login
+      </div>
+    )
+  }
+}
