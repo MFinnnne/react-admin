e4478603b4844dfb84d032019a36b476366b25eb
MFineW <1737374338@qq.com>
2020-10-14 17:26:20 +0800

    增加http测试 更改登录页面

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ import jsonp from 'jsonp';
 
 import ajax from './ajax';
 
-export const reqLogin = (name: string, password: string): Promise<any> => ajax<any>('/api/login', { name, password });
+export const reqLogin = (name: string, password: string): Promise<any> => ajax<any>('/api/user/login', { name, password });
 
 export const reqAddUser = (user: any) => ajax('/manage/user/add', user, 'POST');
 
