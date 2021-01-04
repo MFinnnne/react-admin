6dcd3877ef9be8f5768e671c229c5a5d2d5026e6
MFine <1737374338@qq.com>
2021-01-04 23:46:20 +0800

    角色管理进行中

--- src/pages/role/Role.tsx
+++ src/pages/role/Role.tsx
@@ -1,11 +1,74 @@
-import React, { Component } from 'react'
+import { Button, Card, Table } from 'antd';
+import { ColumnsType } from 'antd/lib/table';
+import React, { Component } from 'react';
+import { RoleModel } from './Model';
 
-export default class Role extends Component {
-  render() {
-    return (
-      <div>
-        Role
-      </div>
-    )
-  }
+interface State {
+	roles: RoleModel[];
+}
+
+interface Props {}
+
+export default class Role extends Component<Props, State> {
+	columns: ColumnsType<any>;
+
+	constructor(props: Props) {
+		super(props);
+		this.columns = [];
+		this.initColumns();
+		this.state = {
+			roles: [{}, {}, {}],
+		};
+	}
+
+	private initColumns = () => {
+		this.columns = [
+			{
+				title: '角色名称',
+				dataIndex: 'name',
+			},
+			{
+				title: '创建时间',
+				dataIndex: 'create_time',
+			},
+			{
+				title: '授权时间',
+				dataIndex: 'auth_time',
+			},
+			{
+				title: '授权人',
+				dataIndex: 'auth_name',
+			},
+		];
+	};
+
+	componentWillMount() {}
+
+	render() {
+		const { roles } = this.state;
+
+		const title = (
+			<span>
+				<Button type="primary">创建角色</Button> &nbsp;&nbsp;
+				<Button type="primary" disabled>
+					设置角色权限
+				</Button>
+			</span>
+		);
+
+		return (
+			<div>
+				<Card title={title}>
+					<Table
+						rowKey="_id"
+						dataSource={roles}
+						columns={this.columns}
+						bordered
+						loading={false}
+						pagination={{ defaultPageSize: 10, showQuickJumper: true }}
+					></Table>
+				</Card>
+			</div>
+		);
+	}
 }
